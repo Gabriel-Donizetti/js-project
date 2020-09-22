b6df46ee7616d28f1813a0a958b4e9d4f011a047
Gabriel-Donizetti <gabsdonizetti@gmail.com>
2020-09-22 15:02:22 -0300

    Project-js/index:add and remove todo

--- Project-js/index.js
+++ Project-js/index.js
@@ -1,7 +1,36 @@
+var ulElement = document.createElement('ul');
+var removeElement = document.createElement('button');
+removeElement.setAttribute('value','Remove');
+removeElement.setAttribute('class','remove');
 
 var buttonElement = document.querySelector('.button');
 buttonElement.onclick = function addTodo() {
     var inputElement = document.querySelector('.input');
-    var text = inputElement.value;
-    console.log(text);
+    var liElement = document.createElement('li');
+    var liText = document.createTextNode(inputElement.value);
+
+    liElement.appendChild(liText);
+    liElement.appendChild(removeElement);
+    ulElement.appendChild(liElement);
+    
 }
+
+var divElement = document.querySelector('#list');
+divElement.appendChild(ulElement);
+
+
+// var botaoElement = document.querySelector('#botao');
+// var ulElement = document.createElement('ul');
+
+// botaoElement.onclick = function createText(){
+//     var inputElment = document.querySelector('#input');
+//     //var inputValue = inputElment.value;
+//     var liElement = document.createElement('li');
+//     var liText = document.createTextNode(inputElment.value)
+//     liElement.appendChild(liText);
+//     ulElement.appendChild(liElement);
+
+// }
+
+// var divElement = document.querySelector('#app');
+// divElement.appendChild(ulElement);
